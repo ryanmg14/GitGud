e4115b2e33c96d349499c878a4ae8e7c7ff5b565
ryanmg14 <ryanmg@bgsu.edu>
2020-02-22 23:18:32 -0500

    Commented out url changes

--- backend/github/views.py
+++ backend/github/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'github/home.html')
 
-def info(request):
-    return render(request, 'github/info.html')
+# def info(request):
+#     return render(request, 'github/info.html')
 
 

fe4174dbf867d40b1301b2ece948c36ca2193911
ryanmg14 <ryanmg@bgsu.edu>
2020-02-23 06:54:22 -0500

    Fixed graph

--- backend/github/static/github/js/main.js
+++ backend/github/static/github/js/main.js
@@ -4,3 +4,59 @@
 //     alert("something");
 //     alert(formSub);
 // }
+
+
+// function mascot(){
+//     var randNum = Math.floor(Math.random() * 5);
+//     var mascotImg
+//     switch (randNum){
+//         case 0 : 
+//             mascotImg = "dino.jpg"
+//             break;
+//         case 1 : 
+//             mascotImg = "bird.png"
+//             break;
+//         case 2 : 
+//             mascotImg = "dino.jpg"
+//             break;
+//         case 3 : 
+//             mascotImg = "bird.png"
+//             break; 
+//         case 4 : 
+//             mascotImg = "dino.jpg"
+//             break;
+//     }
+//     return mascotImg;
+// }
+
+// function generateMascot(mascot){
+//     var img_url =  '/static/github/images/' + mascot;
+//     $("section").append('<img class="images" height=100px src=' + img_url + '>');
+     
+//     var randPos = Math.floor(Math.random() * 500);
+//     $(".images").css({
+//         "top": randPos+"px",
+//         "left": randPos+"px"
+//     });
+
+
+//     var $img = $(".images");
+//     function rotate(degree) {
+//         $img.animate({
+//                     '-webkit-transform': 'rotate(' + degree + 'deg)',
+//                     '-moz-transform': 'rotate(' + degree + 'deg)',
+//                     '-ms-transform': 'rotate(' + degree + 'deg)',
+//                     '-o-transform': 'rotate(' + degree + 'deg)',
+//                     'transform': 'rotate(' + degree + 'deg)',
+//                     'zoom': 1
+//         }, 3000);
+//     }
+//     rotate(30);    
+//     // $('#image').
+// }
+
+// $().ready(function(){
+//     setInterval(function(){ generateMascot(mascot()); }, 3000);
+// })
+
+

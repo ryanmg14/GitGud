813bd807d69e99655d6f160708b1eb4b412dff4a
Rebecca Brunner <brunner@bgsu.edu>
2020-02-22 21:06:11 -0500

    Find languages

--- scraper.py
+++ scraper.py
@@ -8,6 +8,7 @@ class GitHubScraper:
         self.base_url = base_url
         self.num_contributors = 0
         self.commits = []
+        self.languages = []
 
     # Find Number of Contributors
     def find_contributors(self):
@@ -43,9 +44,25 @@ class GitHubScraper:
             except:
                 return
 
+    def find_languages(self):
+        langpage = urllib.request.urlopen(self.base_url)
+        lsoup = BeautifulSoup(langpage, 'html.parser')
+
+        languages = lsoup.find_all(class_='language-color', itemprop='keywords')
+        for lang in languages:
+            dict_lang = {}
+            language = lang.get('aria-label')
+            language = language.split()
+            percent = language[1]
+            language = language[0]
+            dict_lang['language'] = language
+            dict_lang['percent'] = percent
+            self.languages.append(dict_lang)
+
     def parse_site(self):
         self.find_contributors()
         self.find_commits()
+        self.find_languages()
 
 # Test Code
 parser = argparse.ArgumentParser()
@@ -55,4 +72,3 @@ base_url = args.base_url
 
 scraper = GitHubScraper(base_url)
 scraper.parse_site()
-print(scraper.num_contributors, scraper.commits)
